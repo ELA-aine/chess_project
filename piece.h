9dd97f1ebca0ffe532783b04e087bbd3be626fe1
ELA-aine <54710511+ELA-aine@users.noreply.github.com>
2024-07-16 22:50:23 -0400

    small update to piece.h

--- piece.h
+++ piece.h
@@ -1,10 +1,12 @@
 #ifndef PIECE_H
 #define PIECE_H
+#include <string>
+using namespace std;
 
 class Piece {
   protected:
     string colour;
-    int curX, int curY;
+    int curX, curY;
   public:
     Piece(string colour, int curX, int curY);
     virtual bool isValid(int endX, int endY);
